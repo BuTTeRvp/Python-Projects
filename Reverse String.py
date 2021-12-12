4b4712d4a36d78f2a117f88ba2dd8c0effb735c7
Vivek Pandey <56454109+BuTTeRvp@users.noreply.github.com>
2021-12-13 00:00:37 +0530

    Add files via upload

---
+++ Reverse String.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+# Goal -> Reverse the string.
+
+stri = input("Enter A word : ")
+a = ""
+for i in range(1, len(stri) + 1):
+        a += stri[len(stri) - i]
+print(a)
+
+
+# In[ ]:
+
+
+
+
