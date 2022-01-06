2d9eae3b9816c16eed027c1e5e3f907ad697a342
Vivek Pandey <56454109+BuTTeRvp@users.noreply.github.com>
2022-01-06 17:26:21 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,20 @@
+l1 = [2,4,3]
+ 
+l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+
+
+
+l1_r = list(map(lambda x : str(x), l1[::-1]))                         #list[::-1] 
+l2_r = list(map(lambda x : str(x), l2[::-1]))
+
+def int_str(x): 
+    empty_string = ""          # takes ints from a list and concantiates them in a string 
+    for i in x:
+        empty_string += i
+    return int(empty_string)
+x = int_str(l1_r)
+y = int_str(l2_r)
+z = str(x + y)
+print(list(map(lambda x : int(x), z)))
